533b47b502538a6a226fc43ad1936e5c0bcec435
Nihilius <31545101+Nihilius@users.noreply.github.com>
2019-03-07 15:07:43 -0500

    Update form-to-email.php

--- form-to-email.php
+++ form-to-email.php
@@ -1,5 +1,41 @@
+
+// Using Awesome https://github.com/PHPMailer/PHPMailer
 <?php
-if(isset($_POST['xml'])) {
+require 'PHPMailerAutoload.php';
+$xml = $_POST['xml']; // required
+$mail = new PHPMailer;
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+//$Host = 'smtp.mailgun.org';                     // Specify main and backup SMTP servers
+$mail->SMTPAuth = false;                               // Enable SMTP authentication
+//$mail->Username = 'postmaster@YOUR_DOMAIN_NAME';   // SMTP username
+//$mail->Password = 'secret';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable encryption, only 'tls' is accepted
+
+$mail->From = 'sys@sysctl.heroku.app';
+$mail->FromName = 'SysCtl';
+$mail->addAddress('bvelicsw@gmail.com, brandon.velic@sherwin.com, adam.tomson@sherwin.com');                 // Add a recipient
+
+$mail->WordWrap = 100;                                 // Set word wrap to 50 characters
+
+$mail->Subject = 'Sys-Ctl Distribution Request';
+$mail->Body    = $xml;
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
+
+
+
+
+
+
+
+<?php
+/*if(isset($_POST['xml'])) {
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
     $email_to = "bvelicsw@gmail.com, brandon.velic@sherwin.com, adam.tomson@sherwin.com";
@@ -37,5 +73,5 @@ Thank you for contacting us. We will be in touch with you very soon.
 
 <?php
 
-}
+}*/
 ?>
